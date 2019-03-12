acaea2525c7704858a94ac522c837b73dbc33808
PizzaWhisperer <mathilde.raynal@epfl.ch>
2019-03-12 15:28:13 +0100

    added signature in json + test

--- commitments.test.js
+++ commitments.test.js
@@ -2,20 +2,41 @@ const chkGdevP256 = 'BCyENEmEdWz3Wivy7iwXFcLZ0xOW7PCe2BtoMD6sYBqUK+PBZad5euc1tP9
 const chkHdevP256 = 'BJj8hRLfPSe+GNfbS3Jd2XmYU3XTEJw+TaTxx7M9lxVY9BDI6toWVpmffMR0P28XJcV3W0SGWX2OOrRLaBYGhwM=';
 const chkGv1P256 = 'BOidEuO9HSJsMZYE/Pfc5D+0ELn0bqhjEef2O0u+KAw3fPMHHXtVlEBvYjE5I/ONf9SyTFSkH3mLNHkS06Du6hQ=';
 const chkHv1P256 = 'BHOPNAWXRi4r/NEptOiLOp8MSwcX0vHrVDRXv16Jnowc1eXXo5xFFKIOI6mUp8k9/eca5VY07dBhAe8QfR/FSRY=';
+const secretKey = '9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08';
+const signature = [78, -54, 120, 102, -66, 124, 66, 111, -34, -125, 0, -53, 82, -32, 35, 79, -79, -98, -43, 91, -45, -67, 54, 12, -110, -21, -85, -109, -98, -20, -18, -97];
+
 describe('test parsing P256', () => {
     test('dev', () => {
-        const COMMITMENTS = require('./commitments-p256.json');
-        let cfDevG = COMMITMENTS['CF']['dev']['G'];
-        let cfDevH = COMMITMENTS['CF']['dev']['H'];
+        const COMMITMENTS = require('./commitments-p256-signed.json');
+        let cfDevG = COMMITMENTS['commitments']['CF']['dev']['G'];
+        let cfDevH = COMMITMENTS['commitments']['CF']['dev']['H'];
         expect(cfDevG == chkGdevP256).toBeTruthy();
         expect(cfDevH == chkHdevP256).toBeTruthy();
     });
 
     test('v1.0', () => {
-        const COMMITMENTS = require('./commitments-p256.json');
-        let cfDevG = COMMITMENTS['CF']['1.0']['G'];
-        let cfDevH = COMMITMENTS['CF']['1.0']['H'];
+        const COMMITMENTS = require('./commitments-p256-signed.json');
+        let cfDevG = COMMITMENTS['commitments']['CF']['1.0']['G'];
+        let cfDevH = COMMITMENTS['commitments']['CF']['1.0']['H'];
         expect(cfDevG == chkGv1P256).toBeTruthy();
         expect(cfDevH == chkHv1P256).toBeTruthy();
+    });
+    test('signature', () => {
+        const COMMITMENTS = require('./commitments-p256-signed.json');
+
+        const bls = require('bls-lib');
+        bls.onModuleInit(() => {
+          bls.init()
+
+          let s = bls.secretKeyImport(Buffer.from(secretKey, 'hex'));
+          let P = bls.publicKeyImport(Buffer.from(COMMITMENTS['public-key'], 'hex'));
+
+          let sig = bls.signatureImport(Buffer.from(COMMITMENTS['signature']));
+          //sig is well loaded ?
+          expect(bls.signatureExport(sig) == signature).toBeTruthy();
+
+          let msg = JSON.stringify(COMMITMENTS['commitments']);
+          expect(bls.verify(sig, P, msg)).toBeTruthy();
+        });
     });
 });
