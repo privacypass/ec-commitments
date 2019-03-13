041582c707cbe5da2d1670f96464261b35df6906
PizzaWhisperer <mathilde.raynal@epfl.ch>
2019-03-13 10:34:32 +0100

    adds verification ex

---
+++ ver_sig.js
@@ -0,0 +1,18 @@
+const public_key = "cd995480d3287eb8626b1a40b224ed9ada052cae3472665eba297b9785929a1f8ecee5b65dd580f12f239a7183490c4470595ec73cde697cb92d15f1b666c597";
+
+const COMMITMENTS = require('./commitments-p256-signed.json');
+
+const bls = require('bls-lib');
+
+bls.onModuleInit(() => {
+  bls.init()
+  let P = bls.publicKeyImport(Buffer.from(public_key, 'hex'));
+  let sig = bls.signatureImport(Buffer.from(COMMITMENTS['signature']));
+  let msg = JSON.stringify(COMMITMENTS['commitments']);
+  if (bls.verify(sig, P, msg)) {
+    console.log("Signature verifed");
+  }
+  else {
+    console.log("Signature could not be verfified");
+  }
+});
